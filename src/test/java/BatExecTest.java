6d3e80723f57f5f539c9dd58c5d9c01c5c425be2
ScottCTD <ScottCTD@outlook.com>
2020-10-16 18:48:05 +0800

    1.0 Release!!!

--- src/test/java/BatExecTest.java
+++ src/test/java/BatExecTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BatExecTest {
+
+import java.io.IOException;
+
+public class BatExecTest {
+
+    public static void main(String[] args) {
+        String exec = "C:\\Synchronization\\Code\\VocabularyDecoder\\target\\start.bat";
+        try {
+            Process process = Runtime
+                    .getRuntime()
+                    .exec("cmd /c start java -Dfile.encoding=utf-8 -jar VocabularyDecoder-v0.7.jar");
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
 }
